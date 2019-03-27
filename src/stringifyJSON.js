2f11f76e25d83799b195dc8ae01e0d30b0b59eaa
MikeQVo <MikeQVo@gmail.com>
2019-03-27 13:38:56 -0600

    got a working recursion for now but it's not passing

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,48 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  var outputArray = [];
+  var vals = [];
+  var keys = [];
+
+if (typeof obj === 'number' || typeof obj === 'boolean'){
+	return '' + obj;	
+}
+  if(typeof obj === 'string'){
+	return '\"'+ obj +'\"'
+  }
+  if(typeof obj === null){
+  	return 'null'
+  } 
+
+  if(Array.isArray(obj)){
+  	if(obj[0]=== undefined){
+  		return '[]';
+  	} else {
+  		obj.forEach(function(value){
+  			outputArray.push(stringifyJSON(value))
+  		});
+  	} return '[' + outputArray + ']' // '[[x , 1]]''
+  }
+
+  if(obj instanceof Object){
+  	keys = Object.keys(obj);
+  	keys.forEach(function(key){
+  		var eachKey = obj[key];
+  		var outputKey = '\"' + key + '\":';
+  		if(typeof eachKey === 'function' || typeof eachKey === undefined){
+  			outputArray.push('');
+  		} else if (typeof eachKey === 'string'){
+  				outputArray.push(outputKey + '\"' + eachKey + '\"');
+		} else if (eachKey instanceof Object){
+  			outputArray.push(outputKey + stringifyJSON(eachKey)); 
+  		} else if(typeof eachKey === 'number' || typeof eachKey === 'boolean' || typeof eachKey === null){
+  			outputArray.push(outputKey + eachKey)
+  		}
+  	});
+  	 return '{' + outputArray + '}';
+  }
+ 
 };
+
